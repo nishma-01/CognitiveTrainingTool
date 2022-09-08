3cb536a41e1ff332dbb1feb2ae52fee5b5929015
nishma_mehta <nishmamehta1@gmail.com>
2022-09-08 18:22:19 +0100

    homepage components created

---
+++ components/title.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const Title = () => {
+  return (
+     <View>
+      <Text>Cognitive Training Tool</Text>
+    </View>
+  );
+};
+
+export default Title;
+
+const styles = StyleSheet.create({});
--- screens/home.js
+++ screens/home.js
@@ -1,14 +1,34 @@
 import React from "react";
-import { StyleSheet, Text, View } from 'react-native';
+import { ImageBackground, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import Title from "../components/title";
 
 const Home = () => {
   return (
      <View>
-      <Text>Home page</Text>
+      <Title />
+      <View style={styles.container}>
+      <ImageBackground 
+      source={require('../assets/home-img1.png')}
+      style={styles.image}
+      resizeMode='contain'>
+        </ImageBackground>
+      </View>
+      <TouchableOpacity>
+        <Text>Start</Text>
+      </TouchableOpacity>
     </View>
   );
 };
 
 export default Home;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  image: {
+    height: 300,
+    width: 300,
+  },
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
