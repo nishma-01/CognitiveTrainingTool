f78fa4faa6972c9cd5bd64280fab52233559da35
nishma_mehta <nishmamehta1@gmail.com>
2022-09-08 18:51:24 +0100

    results page components created

--- screens/results.js
+++ screens/results.js
@@ -1,14 +1,39 @@
 import React from "react";
-import { StyleSheet, Text, View } from 'react-native';
+import { TouchableOpacity, ImageBackground, StyleSheet, Text, View } from 'react-native';
 
 const Results = () => {
   return (
-     <View>
+  <View>
+    <View> 
       <Text>Results page</Text>
     </View>
+
+    <View style={styles.container}>
+      <ImageBackground 
+        source={require('../assets/home-img1.png')}
+        style={styles.image}
+        resizeMode='contain'>
+      </ImageBackground>
+    </View>
+
+    <View>
+      <TouchableOpacity>
+        <Text>HOME</Text>
+      </TouchableOpacity>
+    </View>
+  </View>
   );
 };
 
 export default Results;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  image: {
+  height: 300,
+  width: 300,
+},
+  container: {
+  justifyContent: 'center',
+  alignItems: 'center',
+},
+});
